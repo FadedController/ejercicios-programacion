3a191a8cbe9f5860261e677b85e52420124e84d4
Axel Padilla <adpadillar25@gmail.com>
2022-05-05 08:25:27 -0600

    Ejercicio 22

--- public/js/nav.js
+++ public/js/nav.js
@@ -7,7 +7,7 @@ const currentExample = parseFloat(
 
 const examples = [
   1, 2, 3, 4, 5, 6, 7, 8.1, 8.2, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-  21,
+  21, 22,
 ];
 
 const prevExample = examples[examples.indexOf(currentExample) - 1];
